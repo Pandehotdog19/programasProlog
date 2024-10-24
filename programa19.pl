3db16c0235d4d643ba6b76ab858101d9d398a9c4
Pandewebo <158231675+Pandehotdog19@users.noreply.github.com>
2024-10-23 21:43:17 -0700

    Update programa19.pl

--- programa19.pl
+++ programa19.pl
@@ -7,6 +7,7 @@
 % ===============================================
 
 % -------- Código en C# (comentado) ------------
+
 % using System;
 % using System.Collections.Generic;
 % 
@@ -39,9 +40,18 @@
 
 % -------- Código en Prolog --------------------
 
+% Divide la lista en dos partes: la primera parte L1 de longitud N1 y la segunda parte L2.
+split([], 0, [], []).
+split(L, N1, L1, L2) :-
+    N1 > 0,
+    L = [H|T],
+    L1 = [H|L1Tail],
+    N2 is N1 - 1,
+    split(T, N2, L1Tail, L2).
+split(L, 0, [], L).
+
 % Rota una lista N lugares a la izquierda.
 % Si N es mayor que la longitud de la lista, utiliza N mod longitud para manejarlo.
-
 programa19(L, N, R) :-
     length(L, Len),        % Obtiene la longitud de la lista.
     N1 is N mod Len,       % Calcula N1 como N mod longitud para manejar rotaciones grandes.
