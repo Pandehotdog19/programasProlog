c92fce2e45e087596cc41ff07e57f17bdd404207
Pandewebo <158231675+Pandehotdog19@users.noreply.github.com>
2024-10-23 22:08:44 -0700

    Update programa25.pl

--- programa25.pl
+++ programa25.pl
@@ -46,31 +46,9 @@
 % -------- Código en Prolog --------------------
 
 % Genera una permutación aleatoria de los elementos de una lista.
-% Utiliza la selección aleatoria para construir la permutación.
 programa25(L, P) :- 
-    length(L, Len),        % Obtiene la longitud de la lista.
-    Len > 0,               % Asegura que la lista no esté vacía.
-    MaxIndex is Len + 1,   % Calcula el límite superior para el índice aleatorio.
-    rnd_select(L, Len, P, MaxIndex). % Selecciona todos los elementos de la lista aleatoriamente.
-
-% Extrae un número dado de elementos aleatorios de una lista.
-% Utiliza un generador de números aleatorios y elimina los elementos seleccionados.
-rnd_select(_, 0, [], _).  % Caso base: si N es 0, la lista resultante es vacía.
-rnd_select(L, N, [X|R], MaxIndex) :- 
-    random(1, MaxIndex, I), % Genera un índice aleatorio entre 1 y MaxIndex.
-    element_at(X, L, I),      % Obtiene el elemento en la posición I de la lista.
-    delete(L, X, L1),         % Elimina el elemento seleccionado de la lista.
-    N1 is N - 1,              % Decrementa N.
-    rnd_select(L1, N1, R, MaxIndex). % Llama recursivamente para seleccionar el resto.
-
-% Obtiene el elemento en la posición I de la lista (1-indexed).
-element_at(X, [X|_], 1).  % Si I es 1, el primer elemento es X.
-element_at(X, [_|T], I) :-  % Si I no es 1, busca en el resto de la lista.
-    I > 1, 
-    I1 is I - 1, 
-    element_at(X, T, I1).
+    random_permutation(L, P). % Usa la función incorporada de Prolog.
 
 % Ejemplo de uso:
 % ?- programa25([1, 2, 3, 4, 5], P).
-% P = [X1, X2, X3, X4, X5].  (donde X1, X2, ..., X5 son los elementos en un orden aleatorio).
-% ----------------------------------------------
+% P = [3, 1, 4, 2, 5]. (Ejemplo de salida aleatoria).
