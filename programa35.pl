d5b0142098a2b5bc96830b33b0a6b23de31a33b6
Pandewebo <158231675+Pandehotdog19@users.noreply.github.com>
2024-10-28 14:40:06 -0700

    Update programa35.pl

--- programa35.pl
+++ programa35.pl
@@ -67,20 +67,25 @@
 es_primo(2).
 es_primo(N) :- 
     N > 2, 
-    \+ (between(2, DivMax, X), N mod X =:= 0),
-    DivMax is floor(sqrt(N)). % Calcula el máximo divisor entero
+    DivMax is floor(sqrt(N)), 
+    \+ (between(2, DivMax, X), N mod X =:= 0).
 
 % Factoriza un número N y devuelve sus factores primos con multiplicidad en una lista
 factorizar(N, L) :- factorizar(N, 2, L).
 
-factorizar(1, _, []) :- !. % Base case: no hay factores en 1
+factorizar(1, []) :- !. % Caso base: no hay factores en 1
 factorizar(N, F, [F|L]) :- 
     N mod F =:= 0, 
     N1 is N // F, 
-    factorizar(N1, F, L). % Si F es un factor, continúa factorizando N1
+    factorizar(N1, F, L). % Si F es un factor, continúa factorizando
 factorizar(N, F, L) :- 
     F1 is F + 1, 
     (es_primo(F1) -> factorizar(N, F1, L) ; factorizar(N, F1, L)). % Incrementa F y continúa
+
+% Interfaz pública para ejecutar el programa
+programa35(N, L) :-
+    N > 1, 
+    factorizar(N, L).
 
 % Ejemplo de uso:
 % ?- programa35(60, L).  % Debería devolver L = [[2, 2], [3, 1], [5, 1]].
